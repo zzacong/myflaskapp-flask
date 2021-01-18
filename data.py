c982f18016b2739add224ae2e3499af6d9765158
zzacong <ongzz100@gmail.com>
2021-01-18 22:29:05 +1100

    getting started

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eligendi fugit neque error nisi tempora cum nulla obcaecati reprehenderit. Id eum delectus, illo quasi quas vitae possimus iusto voluptatum culpa?',
+            'author': 'Tony Stark',
+            'create_date': '10-01-2021'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eligendi fugit neque error nisi tempora cum nulla obcaecati reprehenderit. Id eum delectus, illo quasi quas vitae possimus iusto voluptatum culpa?',
+            'author': 'John Doe',
+            'create_date': '11-01-2021'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eligendi fugit neque error nisi tempora cum nulla obcaecati reprehenderit. Id eum delectus, illo quasi quas vitae possimus iusto voluptatum culpa?',
+            'author': 'Foo Bar',
+            'create_date': '12-01-2021'
+        },
+    ]
+    return articles
